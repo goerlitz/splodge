c5e8d85a96b4b06038473d3c6a0f4baed2f72a99
olaf.goerlitz <olaf.goerlitz@73cb4062-f13d-cb8e-4e61-37df71ad9e5c>
2012-04-30 17:20:02 +0000

    added more scripts

---
+++ trunk/splodge/scripts/sort_cspo.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#------------------------------------------------------------------------------
+# Sort the RDF quads in CSPO order.
+###############################################################################
+
+export LANG=C sort
+
+echo "processing $# files..." >&2;
+for i in $@; do
+  echo ": $i" >&2;
+  # decompress and move context URI to first column
+  gzip -dc $i
+done \
+| awk '{tmp=$(--NF); $(NF)="."; print tmp,$_}' \
+| sort -u -S 3g -T . --compress-program=gzip \
+| awk '{$NF=$1; $1=""; $++NF="."; print }' | sed 's/^ //'
+#| awk '{$NF=$1; $1=""; print $0,"."}' | sed 's/^ //'
+
+#| perl -ne '{m/(.*)(<.*?> )(.*)/;print "$2$1$3\n"}'   # slower
+#| perl -ane '{@ctx=splice(@F,@F-2,1); unshift(@F,@ctx[0]); print "@F\n"}'   # much slower
