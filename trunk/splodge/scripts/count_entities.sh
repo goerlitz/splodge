e638b866014788d2018e9a0353c6c0a567a7a21d
olaf.goerlitz <olaf.goerlitz@73cb4062-f13d-cb8e-4e61-37df71ad9e5c>
2012-05-15 16:53:42 +0000

    updated scripts

---
+++ trunk/splodge/scripts/count_entities.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#------------------------------------------------------------------------------
+# Count the number of entities in RDF triples/quads.
+###############################################################################
+
+echo "CAUTION: duplicates from different chunks are not eliminated yet." >&2;
+echo "processing $# files..." >&2;
+for i in $@; do
+  echo ": $i" >&2;
+  gzip -dc $i \
+  | awk '
+  {
+    # increase counter for subject/object URIs
+    # note: "index($3,"<") == 1" is faster than "$3 ~ /^</" and "substr($3,1,1)"
+
+    if (index($1,"<") == 1) s[$1]++;
+#    if (NF==5 && index($3,"<") == 1) s[$3]++;
+    if (NF==5 && $2 != "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>" && index($3,"<") == 1) s[$3]++;
+  }
+  END {
+    for(i in s) print s[i],i
+  }'
+done \
+| tr -d '<>' \
+| sort -nr -S 2g -T .
+
