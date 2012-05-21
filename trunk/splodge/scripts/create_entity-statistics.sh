a9c4457f93bcef1d95c0b782b29985665087c85d
olaf.goerlitz <olaf.goerlitz@73cb4062-f13d-cb8e-4e61-37df71ad9e5c>
2012-05-21 12:00:52 +0000

    improved script usability

--- trunk/splodge/scripts/create_entity-statistics.sh
+++ trunk/splodge/scripts/create_entity-statistics.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 #------------------------------------------------------------------------------
-# Create entity statistics for incoming and outgoing edges (predicate count).
+# Create entity statistics for incoming and outgoing edges (predicate counts).
 # PARAMS: list of gziped input files.
-# RESULT: statistics file for each input file (<filename>_entity-stats.txt).
+# OUTPUT: statistics file for each input file (<filename>_entity-stats.txt).
 ###############################################################################
-
-export LANG=C;  # speed up sorting
 
 # usage message
 usage() {
@@ -14,25 +12,38 @@ usage: $0 options
 
 OPTIONS:
    -h      Show this message
-   -d      Set predicate dictionary file
+   -p      Set predicate dictionary file
+   -t      Set temp dir (for sort)
 EOF
 }
 
+export LANG=C;  # speed up sorting
+
+STATFILE="entity-stats.gz";
+
 # parse arguments
-while getopts "hd:" OPTION; do
+while getopts "hp:t:" OPTION; do
   case $OPTION in
     h) usage; exit 1 ;;
-    d) DICT=$OPTARG ;;
+    p) PDICT=$OPTARG ;;
+    t) TMP=$OPTARG ;;
   esac
 done
+shift $(( OPTIND-1 )) # shift consumed arguments
 
-# shift arguments by number of consumed options
-shift $(( OPTIND-1 ))
-
-# check settings
-if [ -z "$DICT" ]; then
-  echo "WARNING: a predicate dictionary must be supplied (use -d flag, or -h for help).";
+# check dictionary settings
+if [ -z "$PDICT" ]; then
+  echo "WARNING: a predicate dictionary must be supplied (use -p flag, or -h for help).";
   exit 1;
+fi
+# check temp dir settings
+if [ ! -z "$TMP" ]; then
+  if [ -e "$TMP" ] && [ -d "$TMP" ]; then
+    TMP="-T $TMP";
+  else
+    echo "invalid temp directory: $TMP";
+    exit 1;
+  fi
 fi
 
 # handle all files from argument list
@@ -43,8 +54,8 @@ for i in $@; do
   statfiles=$statfiles" "$outfile;
 
   # check if stat file already exists
-  if [ -e $outfile ]; then
-    echo "skip: '$i' has already been processed.";
+  if [ -e $outfile ] && [ $(stat -c%s "$outfile") -gt "0" ]; then
+    echo "skipped '$i': stat file already exists.";
     continue;
   fi
 
@@ -53,33 +64,43 @@ for i in $@; do
   gzip -dc $i | perl -ne '
   BEGIN {
     # prepare predicate dictionary (predicate -> ID)
-    open FILE, "'"$DICT"'" or die "error loading dictionary "'"$DICT"'": $!";
-    while (<FILE>) { chomp; $dict{"<$_>"} = $. }
+    open FILE, "'"$PDICT"'" or die "error loading dictionary '"$PDICT"': $!";
+    while (<FILE>) { chomp; $pdict{"<$_>"} = $. }
     close FILE;
   } {
     # count predicates (ID) for incoming and outgoing edges of entities (URI/BNode)
-    my ($s, $p, $o, @rest) = split; $p = $dict{$p};
+    my ($s, $p, $o, @rest) = split; $p = $pdict{$p};
     $edge->{$s}[1]->{$p}++;
     $edge->{$o}[0]->{$p}++ if ($#rest == 1 && $o !~ "^\"");
   } END {
     # print predicate lists (IDs) with predicate frequency for incoming and outgoing edges of each entity
     print ((join " ", $_, map {my $plist=$_; defined $_ ? join ",", map {"$_:$plist->{$_}"} keys %$_ : ""} @{$edge->{$_}})."\n") for (keys %$edge);
   }' \
-  | tr -d '<>' | sort >$outfile;  # remove URI brackets and save sorted entitity statistics
+  | tr -d '<>' | sort $TMP >$outfile;  # remove URI brackets and save sorted entitity statistics
 done
 
-# combine generated statistic files and merge duplicates
-echo "merging..." >&2;
-sort -m $statfiles | perl -ne '{
-  chomp; ($uri, @maps) = split /[ ]/;
-  if ($last_uri ne $uri) {
-    print "$last_uri @last_maps\n" if ($last_uri);
-    ($last_uri, @last_maps) = ($uri, @maps);
-    next;
-  }
-  map {map {($key, $val) = split /:/; $data[$idx]->{$key}+=$val;} split /,/; $idx = ++$idx&1; } @maps[0..1], @last_maps[0..1];
-  @last_maps = map {my $plist=$_; defined $_ ? join ",", map {"$_:$plist->{$_}"} sort keys %$_ : ""} @data;
-  @data = ();
-} END {
-  print "$last_uri @last_maps\n";
-}' | gzip >entity-stats_done.gz
+
+# merge statistic files and combine duplicates
+echo `date +%X` "merging entity statistics" >&2;
+
+# check if stat file already exists
+if [ -e $STATFILE ] && [ $(stat -c%s "$STATFILE") -gt "0" ]; then
+  echo "skipped merging: '$STATFILE' already exists.";
+else
+  sort -m $TMP $statfiles | perl -ne '{
+    chomp; ($uri, @stat) = split /[ ]/;
+    if ($last_uri ne $uri) {
+      print "$last_uri @last_stat\n" if ($last_uri);
+      ($last_uri, @last_stat) = ($uri, @stat);
+      next;
+    }
+    map {map {($key, $val) = split /:/; $data[$idx]->{$key}+=$val;} split /,/; $idx = ++$idx&1; } @stat[0..1], @last_stat[0..1];
+    @last_stat = map {my $plist=$_; defined $_ ? join ",", map {"$_:$plist->{$_}"} keys %$_ : ""} @data;
+    @data = ();
+  } END {
+    print "$last_uri @last_stat\n";
+  }' | gzip >$STATFILE
+
+  echo `date +%X` "done. entity statistics written to $STATFILE" >&2;
+fi
+
