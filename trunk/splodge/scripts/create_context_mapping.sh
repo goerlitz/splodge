f258101116179f40ddc540bab52eb215cad1a4e3
olaf.goerlitz <olaf.goerlitz@73cb4062-f13d-cb8e-4e61-37df71ad9e5c>
2012-05-23 23:09:33 +0000

    completed rewriting of context reduction script

--- trunk/splodge/scripts/create_context_mapping.sh
+++ trunk/splodge/scripts/create_context_mapping.sh
@@ -1,34 +1,39 @@
 #!/bin/sh
 #------------------------------------------------------------------------------
-# Create a mapping to normalize full context URIs to a short data source URI.
+# Aggregate domains with multiple subdomains, e.g. {bob,jane}.livejournal.com.
+# 1. step: collect statistics: frequency, domain name, and list of subdomains.
+# 2. step: condense quad context to common domain names.
 ###############################################################################
+
+# set default statistic file
+STATFILE="context-stats.txt";
 
 # handle all files from argument list
 echo "processing $# files..." >&2;
-for i in $@; do
-  echo `date +%X` "$i" >&2;
-  gzip -dc $i
-done \
-| awk '{print $(NF-1)}' | awk -F "/" '{print $3}' | perl -lne '
+
+# check if output statistic file already exists
+if [ -e $STATFILE ] && [ $(stat -c%s "$STATFILE") -gt "0" ]; then
+  echo "skipped analysis: '$STATFILE' already exists.";
+else
+  for i in $@; do
+    echo `date +%X` "$i" >&2;
+    gzip -dc $i
+  done \
+  | awk '{print $(NF-1)}' | awk -F "/" '{print $3}' | perl -lne '
 BEGIN {
   $tree = {};
   sub sum { my $ref = shift; my $sum = 0; $sum += ($_ ? &sum($ref->{$_}) : $ref->{$_}) for (keys %$ref); return $sum };
   sub dom { my $ref = shift; return map { my $name = $_; map { $_ ? "$_.$name" : $name } dom($ref->{$_}) if $_ } keys %$ref };
-  sub aggr {
+  sub aggregate {
     my ($ref, @names) = @_; 
     my @keys = keys %$ref;
 
-#    if (@keys > 1 && @names > 1 && (@names > 2 || $names[0] ne "au" && $names[0] ne "uk" && $names[0] ne "jp")) {
     if (@keys > 1 && @names > 1 && (@names > 2 || $names[0] !~ /^(au|uk|jp)$/)) {
+      # aggregate domains which have multiple subdomains
       print sum($ref)," ",(join ".", reverse @names)," ",join ",", dom($ref);
     } else {
-      for (@keys) {
-        if ($_) {
-          aggr($ref->{$_}, @names, $_);
-        } else {
-          print sum($ref)," ",join ".", reverse @names;
-        }
-      }
+      # process all subdomains recursively or print details of fully traversed domain name
+      for (@keys) { $_ ? aggregate($ref->{$_}, @names, $_) : print sum($ref)," ",join ".", reverse @names }
     }
   }
 } {
@@ -37,7 +42,30 @@ BEGIN {
   map {$ref->{$_} = {} unless $ref->{$_}; $ref = $ref->{$_}} reverse split /\./; 
   $ref->{""}++;
 } END {
-  aggr($tree);
+  aggregate($tree);
 }' \
-| sort -nr >context-stats.txt
+  | sort -nr >$STATFILE
+  echo `date +%X` "done. context statistics written to $STATFILE" >&2;
+fi
 
+# replace quad context with condensed context
+# handle all files from argument list
+for i in $@; do
+  out=${i%.*}"_condensed-uris."${i##*.};
+  echo `date +%X` "updating: $i -> $out" >&2;
+  gzip -dc $i | perl -lne '
+  BEGIN {
+    # prepare predicate dictionary (predicate -> ID)
+    open FILE, "'"$STATFILE"'" or die "error loading dictionary '"$STATFILE"': $!";
+    while (<FILE>) {
+      chomp;
+      my ($count, $host, $subdomains) = split;
+      if ($subdomains) {$dict{"$_.$host"} = $host for (split /,/, $subdomains)};
+    }
+    close FILE;
+  } {
+    my ($first, $host) = m!(.*<.*?//)(.*?)/!;
+    print "$first".(exists($dict{$host}) ? $dict{$host} : $host)."/> .";
+  }' | gzip >$out;
+done
+echo `date +%X` "done. all contexts updated." >&2;
