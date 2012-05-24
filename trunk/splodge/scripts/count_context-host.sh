e27f7b347f8aecd4790576239b60132f0bff5a49
olaf.goerlitz <olaf.goerlitz@73cb4062-f13d-cb8e-4e61-37df71ad9e5c>
2012-05-24 12:28:53 +0000

    small improvements for count scripts

--- trunk/splodge/scripts/count_context-host.sh
+++ trunk/splodge/scripts/count_context-host.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #------------------------------------------------------------------------------
-# Count the number of occurences of raw contexts in RDF quads.
+# Count the number of occurences of context hosts in RDF quads.
 ###############################################################################
 
 echo "processing $# files..." >&2;
@@ -9,7 +9,7 @@ for i in $@; do
   echo ": $i" >&2;
   gzip -dc $i;
 done \
-| awk '{print $(NF-1)}' | awk -F "/" '{c[$3]++} END {for(i in c) print c[i],i}' | sort -rn
-#| awk '{print $(NF-1)}' | awk -F "/" '{print $3}' | perl -ne '{$c{$_}++} END {print "$c[$_] $_" for keys %c}' | sort -rn
-#| awk '{print $(NF-1)}' | perl -ne '{$c{(split /\//)[2]}++} END {print "$c[$_] $_\n" for keys %c}' | sort -rn
-#| awk '{split($(NF-1),a,"/"); c[a[3]]++} END {for(i in c) print c[i],i}' | sort -rn   # 4 times slower
+| awk '{print $(NF-1)}' | awk -F "/" '{c[$3]++} END {for(i in c) print c[i],i}' | sort -nr
+#| awk '{print $(NF-1)}' | awk -F "/" '{print $3}' | perl -ne '{$c{$_}++} END {print "$c[$_] $_" for keys %c}' | sort -nr
+#| awk '{print $(NF-1)}' | perl -ne '{$c{(split /\//)[2]}++} END {print "$c[$_] $_\n" for keys %c}' | sort -nr
+#| awk '{split($(NF-1),a,"/"); c[a[3]]++} END {for(i in c) print c[i],i}' | sort -nr   # 4 times slower
--- trunk/splodge/scripts/count_context-raw.sh
+++ trunk/splodge/scripts/count_context-raw.sh
@@ -3,10 +3,12 @@
 # Count the number of occurences of raw contexts in RDF quads.
 ###############################################################################
 
+export LANG=C;  # speed up sorting
+
 echo "processing $# files..." >&2;
 
 for i in $@; do
-  echo ": $i" >&2;
+  echo `date +%X` "$i" >&2;
   gzip -dc $i;
 done \
-| awk '{c[$(NF-1)]++} END {for(i in c) print c[i],i}' | perl -pe 's/[<>]//g' | sort -rn
+| awk '{c[$(NF-1)]++} END {for(i in c) print c[i],i}' | tr -d '<>' | sort -nr
