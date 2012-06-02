24bc799d181112b3d0c84964448a913de734c4c5
olaf.goerlitz <olaf.goerlitz@73cb4062-f13d-cb8e-4e61-37df71ad9e5c>
2012-06-02 10:03:13 +0000

    improved query generation script usage

--- trunk/splodge/scripts/query_gen.sh
+++ trunk/splodge/scripts/query_gen.sh
@@ -20,7 +20,7 @@ EOF
 
 # set default output files
 PATHFILE="path-stats.gz";
-PREDFILE="predicate-stats.gz";
+PSTAT="predicate-stats.gz";
 COUNT=10;
 
 # parse arguments
@@ -37,7 +37,7 @@ shift $(( OPTIND-1 )) # shift consumed arguments
 
 # check dictionary settings
 if [ -z "$PDICT" ]; then
-  echo "WARNING: a predicate dictionary must be supplied (use -p flag, or -h for help).";
+  echo "WARNING: a predicate list must be supplied (use -p flag, or -h for help).";
   exit 1;
 fi
 if [ -z "$PSTAT" ]; then
@@ -48,26 +48,26 @@ fi
 
 # query generation.
 perl -sle '
+  # DEFINE FUNCTIONS
+
+  # choose p1, c1, p2, c2 randomly (recursive traversal of stats in hash tree)
+  sub choose { my $ref=@_[0]; return (@_ && ref($ref) eq "HASH") ? map {$_, &choose($ref->{$_})} (keys %$ref)[int(rand(keys %$ref))] : () }
+
   # INITIALIZE STATISTICS
   $time = `date +%X`; chomp($time); print STDERR "$time loading predicate and path statistics";
 
   # prepare predicate dictionary (ID -> predicate)
-  open FILE, "'"$PDICT"'" or die "ERROR: cannot load dictionary '"$PDICT"': $!";
+  open FILE, "'"$PDICT"'" or die "ERROR: cannot load predicate list '"$PDICT"': $!";
   while (<FILE>) { chomp; $pindex{"$."} = "<$_>" }
   close FILE;
   # prepare predicate statistics (predicate -> ID, stats)
-  open FILE, "'"$PSTAT"'" or die "ERROR: cannot load predicate statistics '"$PSTAT"': $!";
+  open FILE, "gzip -dc '"$PSTAT"'|" or die "ERROR: cannot load predicate statistics '"$PSTAT"': $!";
   while (<FILE>) { chomp; ($p, $c, $s, $stats) = split; for (split /,/, $stats) { my ($ctx, $n) = split /:/; $pstats->{$p}->{$ctx}=$n } }
   close FILE;
   # prepare predicate statistics (predicate -> ID, stats)
   open FILE, "gzip -dc '"$PATHFILE"'|" or die "ERROR: cannot load path statistics '"$PATHFILE"': $!";
   while (<FILE>) { chomp; ($p1, $p2, $c1, $c2, @counts) = split; @{$stat->{$p1}->{$c1}->{$p2}->{$c2}} = @counts if ($c1 != $c2); }
   close FILE;
-
-  # DEFINE FUNCTIONS
-
-  # choose p1, c1, p2, c2 randomly (recursive traversal of stats in hash tree)
-  sub choose { my $ref=@_[0]; return (@_ && ref($ref) eq "HASH") ? map {$_, &choose($ref->{$_})} (keys %$ref)[int(rand(keys %$ref))] : () }
 
   # GENERATE QUERIES
 
@@ -78,13 +78,18 @@ perl -sle '
     my ($p1, $c1, $p2, $c2) = &choose($stat);
     my $ref = $stat->{$p1}->{$c1}->{$p2}->{$c2};
 
+    # check if current path pattern connected with another pattern spans three sources, otherwise retry
+    # ToDo: prevent endless loop if not possible
+    # ToCheck: are there different possible intermediate sources to connect p1@c1 with p3@c3 via p2?
     if ($stat->{$p2} && $stat->{$p2}->{$c2}) {
       my ($p3, $c3) = &choose($stat->{$p2}->{$c2});
       redo if ($c1 == $c3);
-      # are there different sources to reach p3@c3 from p1@c1 via p2?
 
-#      printf("%5d<%3d> - %5d<%3d> - %5d<%3d> : %d / %d / %d\n", $p1, $c1, $p2, $c2, $p3, $c3, $pstats->{$p2}->{$c2}, $ref->[2], $stat->{$p2}->{$c2}->{$p3}->{$c3}[1]);
+      # print SPARQL query
       printf("SELECT * WHERE {\n  ?var1 %s ?var2 .\n  ?var2 %s ?var3 .\n  ?var3 %s ?var4 .\n}\n", map {$pindex{$_}} $p1, $p2, $p3);
+
+      # print path with pattern counts
+#      printf("%5d<%3d> - %5d<%3d> - %5d<%3d> : %d / %d / %d\n", $p1, $c1, $p2, $c2, $p3, $c3, $pstats->{$p2}->{$c2}, $ref->[2], $stat->{$p2}->{$c2}->{$p3}->{$c3}[1]);
     } else {
       redo;
     }
