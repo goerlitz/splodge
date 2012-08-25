de1befe1b6d79aeb8e97b16e9dff99fe3c18b67e
olaf.goerlitz@gmail.com <olaf.goerlitz@gmail.com@73cb4062-f13d-cb8e-4e61-37df71ad9e5c>
2012-08-25 22:02:37 +0000

    updated documentation

--- trunk/splodge/src/de/uniko/west/splodge/PathStatistics.java
+++ trunk/splodge/src/de/uniko/west/splodge/PathStatistics.java
@@ -1,8 +1,23 @@
+/*
+ * This file is part of SPLODGE.
+ * Copyright 2012 Olaf Goerlitz
+ *
+ * SPLODGE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPLODGE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SPLODGE.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.uniko.west.splodge;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -11,6 +26,11 @@ import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
 
+/**
+ * Container for managing path statistics.
+ * 
+ * @author Olaf Goerlitz (goerlitz@uni-koblenz.de)
+ */
 public class PathStatistics {
 	
 	private Random rand;
--- trunk/splodge/src/de/uniko/west/splodge/QueryGen.java
+++ trunk/splodge/src/de/uniko/west/splodge/QueryGen.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of SPLODGE.
+ * Copyright 2012 Olaf Goerlitz
+ *
+ * SPLODGE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPLODGE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SPLODGE.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.uniko.west.splodge;
 
 import java.io.BufferedReader;
@@ -16,7 +33,7 @@ import java.util.Random;
 import java.util.zip.GZIPInputStream;
 
 /**
- * SPLODGE Query Generator
+ * SPLODGE Query Generator.
  * 
  * @author Olaf Goerlitz (goerlitz@uni-koblenz.de)
  */
